4eff0306d73b49bd312b04fd6b789cf76194c760
468SiliconWP445 <hotlap8878@gmail.com>
2025-01-16 21:22:01 +0000

    Create time.asm

---
+++ time.asm
@@ -0,0 +1,15 @@
+.intel_syntax noprefix
+.global main
+.extern ExitProcess
+
+main:
+    xor rax, rax
+    mov rcx, 100000000
+
+loop_start:
+    add rax, rcx
+    dec rcx
+    jnz loop_start
+
+    mov rdi, 0
+    call ExitProcess
